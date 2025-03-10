bf431abe14914d5f00619a1599ecb6b55bd7e797
Thalia Arcanjo <arcanjotha2@gmail.com>
2025-03-10 11:40:21 -0300

    Desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Mundo!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            scanner.useLocale(Locale.forLanguageTag("pt_BR"));
+
+            System.out.print("Por favor, digite o número da conta: ");
+            int numero = scanner.nextInt();
+
+            System.out.print("Por favor, digite o número da Agência  ");
+            String agencia = scanner.next();
+
+            System.out.print("Por favor, digite o nome do titular da conta: ");
+            String titular = scanner.next();
+
+            System.out.print("Por favor, digite o saldo da conta: ");    
+            double saldo = scanner.nextDouble();
+
+            scanner.close();
+            System.out.println("Ola " + titular + ", obrigado por criar uma conta em nosso banco, sua agencia e " + agencia + ", conta " + numero + " e seu saldo " + saldo + " ja esta disponivel para saque");
+        }
     }
 }
